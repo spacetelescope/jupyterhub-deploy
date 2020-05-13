02a4431cf037e6ec80d5e3b213db47e5b9814f21
YuviPanda <yuvipanda@gmail.com>
2020-05-13 19:34:15 +0530

    Revert all changes to wfirst-dev docker image
    Let's work things back in one step at a time

--- deployments/wfirst-sit/image/mirage-activate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export WEBBPSF_PATH=/data/webbpsf
-export MIRAGE_DATA=/data/mirage
-export PYSYN_CDBS=/data/pysynphot
--- deployments/wfirst-sit/image/wfirst-activate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export pandeia_refdata=/data/pandeia
-export WEBBPSF_PATH=/data/webbpsf
-export PYSYN_CDBS=/data/pysynphot
