9ba0d72c4a1723fd8d2cf3031469b2366924f8ae
YuviPanda <yuvipanda@gmail.com>
2020-05-13 19:34:15 +0530

    Revert all changes to wfirst-dev docker image
    Let's work things back in one step at a time

--- deployments/wfirst-sit/image/apply_home_template.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo -u jovyan rsync -url /opt/home_template/ /home/jovyan/
-sudo -u jovyan mkdir -p /home/jovyan/example_notebooks
-sudo -u jovyan rsync -url /opt/pandeia-coronagraphy/notebooks/ /home/jovyan/example_notebooks/
