7182ac29cf4b4cef5c15f0b2e3e167f1ad64c649
EC2 Default User <ec2-user@ip-10-142-24-89.ec2.internal>
2021-12-20 21:04:54 +0000

    add unique users report

--- tools/get-datadog-user-metrics.py
+++ tools/get-datadog-user-metrics.py
@@ -2,14 +2,16 @@ import os
 import subprocess
 import json
 import math
+import random
+import string
+import operator
+import argparse
 
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from datadog_api_client.v1 import ApiClient, Configuration
 from datadog_api_client.v1.api.metrics_api import MetricsApi
 
-
-# TODO: specify start/end date
 
 
 def get_key(key_type):
@@ -25,7 +27,7 @@ def get_key(key_type):
 
 
 def get_raw_data(query, end_date):
-    end_date = f'{end_date}:00:00:00' # GMT
+    end_date = f'{end_date}:23:59:59' # GMT
     to = datetime.strptime(end_date, '%Y-%m-%d:%H:%M:%S')
     _from = to + relativedelta(weeks=-1)
 
@@ -41,28 +43,54 @@ def get_raw_data(query, end_date):
 
 
 def parse_data(data):
-    users = {}
+    users_raw = {}
     for s in data['series']:
         user = s['tag_set'][0].lstrip('pod_name:')
-        users[user] = []
+        users_raw[user] = []
         for p in s['pointlist']:
             val = p.value[1]
             if val:
-                users[user].append(val)
+                users_raw[user].append(val)
 
-    for u in users:
-        hours = math.ceil(float(len(users[u])))
+    users = {}
+    for u in users_raw:
+        hours = math.ceil(float(len(users_raw[u])))
+        #anon_user = ''.join(random.choice(u) for i in range(8))
+        anon_user = ''.join(random.choice(string.ascii_letters) for i in range(6))
+        users[anon_user] = hours
 
-        print(hours, u)
+    return users
+
+
+def print_report(data, end_date):
+    _sorted = sorted(data.items(), key=operator.itemgetter(1), reverse=True)
+
+    title = f'\nUser data for week ending on {end_date}'
+    print(title)
+    print('-'*len(title))
+    print(f'\nNumber of unique users: {int(len(_sorted))}\n')
+    print('Total usage hours per user (anonymized):')
+    for (u, val) in _sorted:
+        print(f'\t{u}\t{val}')
+    print()
+
+
+
+
+
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--end-date', type=str, required=True)
+    args = parser.parse_args()
+
     os.environ['DD_SITE'] = 'datadoghq.com'
     os.environ['DD_API_KEY'] = get_key('api')
     os.environ['DD_APP_KEY'] = get_key('app')
 
     # TODO: make this an arg...
-    end_date = '2021-12-20'
+    #end_date = '2021-12-20'
 
     # TODO: get these vars elsewhere (arg? env?)
     account = 'aws-account-name:aws-tess-tike-ops'
@@ -72,10 +100,12 @@ if __name__ == '__main__':
 
     raw_data = get_raw_data(
 		query,
-        end_date
+        args.end_date
     )
 
-    parse_data(raw_data)
+    user_data = parse_data(raw_data)
+
+    print_report(user_data, args.end_date)
 
 
 
